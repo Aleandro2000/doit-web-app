f3e9721855808db9d251fea7c06fbf6dcf055494
Aleandro2000 <carmicialexandru@yahoo.com>
2021-04-19 01:24:53 +0300

    doit

--- back/auth/deleteAccount.js
+++ back/auth/deleteAccount.js
@@ -4,7 +4,7 @@ module.exports = function(req, res, next) {
     if(req.session.user&&req.cookies.session)
         User.findByIdAndDelete(req.session.user._id, (err,result)=>{
             if(err)
-                return res.sendStatus(500).send({msg: "Failed deleting account!"});
+                return res.sendStatus(500).send({msg:"Failed deleting account!"});
             res.clearCookie('session');
             return res.status(200).send({msg:"Succesful deleted account!"});
         });
