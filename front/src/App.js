093dfa0303f037c7ed902d219f331afabb1273a1
Aleandro2000 <carmicialexandru@yahoo.com>
2021-04-19 12:12:40 +0300

    doit

--- front/src/App.js
+++ front/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <>
       <nav className="navigation">
         <section className="container">
-            <img className="rotation" src={logo}/>
+          <img className="rotation" src={logo}/>
           <ul className="navigation-list float-right">
              <li className="navigation-item"><a className="navigation-link button button-outline" href="/list">LOGIN</a></li>
              <li className="navigation-item"><a className="navigation-link button button-outline" href="/login">REGISTER</a></li>
