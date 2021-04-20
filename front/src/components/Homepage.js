3b70e63a533faf7333382380c6b240d209cb6e1b
Aleandro2000 <carmicialexandru@yahoo.com>
2021-04-20 16:53:53 +0300

    doit

--- front/src/components/Homepage.js
+++ front/src/components/Homepage.js
@@ -1,22 +1,28 @@
 import "milligram";
+import AOS from "aos";
+import "../aos.css";
 import "./Homepage.css";
 import logo from "../images/logo.png";
+import code from "../images/code.svg";
 
 function Homepage()
 {
+    AOS.init({
+        duration: 1000
+    });
     return(
         <>
             <nav className="navigation">
                 <section className="container">
-                    <img className="rotation" src={logo}/>
-                    <ul className="navigation-list float-right">
+                    <img className="rotation fadeIn" src={logo}/>
+                    <ul className="navigation-list float-right fadeIn">
                         <li className="navigation-item"><a className="navigation-link button-white" href="/list">Login</a></li>
                         <li className="navigation-item"><a className="navigation-link button-white" href="/login">Register</a></li>
                     </ul>
                 </section>
             </nav>
             <div className="header">
-                <div className="header-content">
+                <div className="header-content fadeIn">
                     <b className="header-content-title">
                         Become the Super Software Engineer!
                         <hr/>
@@ -35,8 +41,27 @@ function Homepage()
                     </p>
                 </div>
             </div>
-            <div className="container">
-                Lorem impus dolor sit alem
+            <div className="container content" data-aos='fade-up'>
+                <div className="row">
+                    <div className="column">
+                        <h1 align="center">
+                            You write, we guide
+                        </h1>
+                        <p align="justify">
+                            We helps you to translate ideas and thinking into code, guiding you step by step. Also, we learn you the best skill for today's word. We learn you how to learn.
+                            <br/><br/>
+                            So, keep it clean and simple! We all hate red flags, don’t we? And choosing a bad variable name or overcomplicating the code is one of the most common mistakes in a job interview.
+                            <br/><br/>
+                            A picture is worth a thousand words. But a video offers you more than that. DoIT’s materials content doesn’t just hand you over the solution, it will get you in a thinking process adventure where you will easily develop your problem-solving skills. You'll be able to solve any problem you'll face during an interview.
+                        </p>
+                    </div>
+                    <div className="column">
+                        <p align="center">
+                            <img src={code} style={{filter: "grayscale(75%)"}}/>
+                        </p>
+                        <hr/>
+                    </div>
+                </div>
             </div>
         </>
     );
