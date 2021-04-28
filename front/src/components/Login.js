ea07fe3762085b4a774fb2528256dde48a58c1e3
Aleandro2000 <carmicialexandru@yahoo.com>
2021-04-28 13:02:47 +0300

    doit

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -1,39 +1,57 @@
-import "./Login.css"
-import logo from "../images/logo2.png"
+import Register from "./Register"
+import "./Login.css";
+import logo from "../images/logo2.png";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function Login()
 {
     return(
-        <div className="login">
-            <img src={logo} className="logo"/>
-            <h3 className="title">
-                <b>
-                    Login
-                </b>
-            </h3>
-            <hr/>
-            <form>
-                <label for="email">
-                    Email
-                    <input type="email" name="email" />
-                </label>
-                <label for="password">
-                    Password
-                    <input type="password" name="password" />
-                </label>
-                <input type="submit" value="LOGIN" />
-            </form>
-            <hr/>
-            <button className="button">
-                REGISTER
-            </button>
-            <br/><br/>
-            <p>
-                <b>
-                    © Powered by <i><u>Softana</u></i>, All right reserved.
-                </b>
-            </p>
-        </div>
+        <Router>
+            <Switch>
+                <Route path="/register">
+                    <Register/>
+                </Route>
+                <Route path="/login">
+                    <div className="login">
+                        <img src={logo} className="logo"/>
+                        <h3 className="title">
+                            <b>
+                                Login
+                            </b>
+                        </h3>
+                        <hr/>
+                        <form>
+                            <label for="email">
+                                Email
+                                <input type="email" name="email" />
+                            </label>
+                            <label for="password">
+                                Password
+                                <input type="password" name="password" />
+                            </label>
+                            <input type="submit" value="LOGIN" />
+                        </form>
+                        <hr/>
+                        <Link to="/register">
+                            <button className="button">
+                                REGISTER
+                            </button>
+                        </Link>
+                        <br/><br/>
+                        <p>
+                            <b>
+                                © Powered by <i><u>Softana</u></i>, All right reserved.
+                            </b>
+                        </p>
+                    </div>
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
