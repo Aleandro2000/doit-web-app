50a5e0752d3f9b7350347153b0df68a323c9085e
Aleandro2000 <carmicialexandru@yahoo.com>
2021-04-28 03:19:24 +0300

    doit

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -6,12 +6,33 @@ function Login()
     return(
         <div className="login">
             <img src={logo} className="logo"/>
-            <h1 className="title">
+            <h3 className="title">
                 <b>
                     Login
                 </b>
-            </h1>
+            </h3>
             <hr/>
+            <form>
+                <label for="email">
+                    Email
+                    <input type="email" name="email" />
+                </label>
+                <label for="password">
+                    Password
+                    <input type="password" name="password" />
+                </label>
+                <input type="submit" value="LOGIN" />
+            </form>
+            <hr/>
+            <button className="button">
+                REGISTER
+            </button>
+            <br/><br/>
+            <p>
+                <b>
+                    © Powered by <i><u>Softana</u></i>, All right reserved.
+                </b>
+            </p>
         </div>
     );
 }
