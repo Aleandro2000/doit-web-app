bb78931b8806a76bc2e30ef323745faf85aa7a88
Aleandro2000 <carmicialexandru@yahoo.com>
2021-05-25 19:34:30 +0300

    doit

--- front/src/components/dashboardOptions/IDE.js
+++ front/src/components/dashboardOptions/IDE.js
@@ -1,25 +1,56 @@
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-c_cpp";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/theme-tomorrow_night";
 import "ace-builds/src-noconflict/ext-language_tools";
 import swal from 'sweetalert';
 
 import { Redirect, Link } from "react-router-dom";
 import { createRef, useState } from "react";
-
-function onChange(newValue)
-{
-    console.log("change", newValue);
-}
+import Select from 'react-select';
 
 function IDE()
 {
-    const [filename,setFileName]=useState("code.cpp");
+    const [filename,setFileName]=useState("code.c");
+    const [language,setLanguage]=useState("c_cpp");
     const session=localStorage.getItem("session");
     const aceEditorRef=createRef();
 
+    const options = [
+        { value: '.c', label: 'C' },
+        { value: '.cpp', label: 'C++' },
+        { value: '.java', label: 'Java' },
+        { value: '.py', label: 'Python' },
+    ];
+
+    const [selectedOption, setSelectedOption] = useState(options[0]);
+
     if(!session)
         return <Redirect to="/login" />;
+    
+    const handleChange = (selectedOption) => {
+        setFileName("code"+selectedOption.value);
+        switch(selectedOption.label)
+        {
+            case "C++":
+                setLanguage("c_cpp")
+                break;
+            case "C":
+                setLanguage("c_cpp")
+                break;
+            case "Java":
+                setLanguage("java")
+                break;
+            case "Python":
+                setLanguage("python")
+                break;
+            default:
+                break;
+
+        }
+        setSelectedOption(selectedOption);
+    }
 
     const SaveFile = () => {
         const code=aceEditorRef.current.editor.getValue();
@@ -53,6 +84,23 @@ function IDE()
             });
     }
 
+    const Run = () => {
+        const code=aceEditorRef.current.editor.getValue();
+        if(code)
+        {
+            //TO DO
+        }
+        else
+            swal({
+                title: "ERROR!",
+                text: "IDE Editor may not be empty!",
+                icon: "error",
+                buttons: {
+                    confirm: {text:'OK',className:'alert-button'}
+                }
+            });
+    }
+
     return(
         <>
             <div style={{backgroundColor: "#282a2e"}}>
@@ -67,9 +115,8 @@ function IDE()
                 </div>
                 <div style={{paddingTop: "15px"}}>
                     <AceEditor
-                        mode="c_cpp"
+                        mode={language}
                         theme="tomorrow_night"
-                        onChange={onChange}
                         name="ide-editor"
                         editorProps={{ $blockScrolling: true }}
                         width="100%"
@@ -84,18 +131,21 @@ function IDE()
                         ref={aceEditorRef}
                     />
                 </div>
+                <Select isSearchable={false} options={options} value={selectedOption} onChange={handleChange}/>
                 <div className="ide-options">
                     <p align="right">
                         <button onClick={SaveFile} className="button-white responsive-no-button-border" style={{borderRadius: "0"}}>
                             <i className="fa fa-save"/>|SAVE
                         </button>
-                        <button className="button-white responsive-no-button-border" style={{borderRadius: "0"}}>
+                        <button onClick={Run} className="button-white responsive-no-button-border" style={{borderRadius: "0"}}>
                             <i className="fa fa-cog"/>|RUN
                         </button>
                     </p>
-                    <b>
-                       Console:<span className="prompt-cursor"/>
-                    </b>
+                    <p>
+                        <b>
+                           Console:<span className="prompt-cursor"/>
+                        </b>
+                    </p>
                 </div>
             </div>
             <br/>
