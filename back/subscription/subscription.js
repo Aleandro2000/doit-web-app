35df8e33996d90f52b9bae59b8691417e2512702
Aleandro2000 <carmicialexandru@yahoo.com>
2021-07-13 04:14:54 +0300

    doit

--- back/subscription/subscription.js
+++ back/subscription/subscription.js
@@ -7,52 +7,54 @@ module.exports = function(req,res){
             res.send({title: "ERROR!", message: "Payment request failed!", icon: "error"})
         else
         {
-            let subscriptionPrice;
-            //let price;
+            let subscriptionPrice, price;
             switch(req.body.subscriptionType)
             {
                 case "monthly":
                     subscriptionPrice=process.env.DOIT_MONTHLY_SUBSCRIPTION;
-                    //price=5999;
+                    price=process.env.MONTHLY_PRICE;
                     break;
                 case "yearly":
                     subscriptionPrice=process.env.DOIT_YEARLY_SUBSCRIPTION;
-                    //price=29999;
+                    price=process.env.YEARLY_PRICE;
                     break;
                 default:
                     res.send({title: "ERROR!", message: "Payment request failed!", icon: "error"});
             }
-            /*stripe.paymentIntents.create({
-                amount: pice,
+            stripe.paymentIntents.create({
+                amount: price*100,
                 currency: "usd",
                 payment_method_types: ['card']
-            });*/
-            stripe.customers.create({
-                email: user.email,
-                description: "DoIT "+user.subscriptionType+" subscription",
-                invoice_settings: {
-                    default_payment_method: req.body.payment_method,
-                },
-                payment_method: req.body.payment_method
             })
-                .then(customer => {
-                    stripe.subscriptions.create({
-                        customer: customer.id,
-                        items: [
-                            { price: subscriptionPrice }
-                        ]
+                .then(paymentIntent => {
+                    stripe.customers.create({
+                        email: user.email,
+                        description: "DoIT "+user.subscriptionType+" subscription",
+                        invoice_settings: {
+                            default_payment_method: req.body.payment_method,
+                        },
+                        payment_method: req.body.payment_method
                     })
-                        .then(subscription => {
-                            user.subscribedAt=Date.now();
-                            user.customerId=customer.id;
-                            user.subscriptionId=subscription.id;
-                            user.save();
-                            res.status(200).send({title: "CONGRATULATIONS!", message: "Payment request successfully!", icon: "success", result: user});
+                        .then(customer => {
+                            stripe.subscriptions.create({
+                                customer: customer.id,
+                                items: [
+                                    { price: subscriptionPrice }
+                                ]
+                            })
+                                .then(subscription => {
+                                    user.subscribedAt=Date.now();
+                                    user.customerId=customer.id;
+                                    user.subscriptionId=subscription.id;
+                                    user.save();
+                                    res.status(200).send({title: "CONGRATULATIONS!", message: "Payment request successfully!", icon: "success", result: user});
+                                })
+                                .catch(err => res.send({title: "ERROR!", message: "Payment request failed!", icon: "error"}));
+                            
                         })
                         .catch(err => res.send({title: "ERROR!", message: "Payment request failed!", icon: "error"}));
-                    
                 })
-                .catch(err => res.send({title: "ERROR!", message: "Payment request failed!", icon: "error"}));
+                .catch(err => res.send({title: "ERROR!", message: err.code, icon: "error"}));
         }
     });
 }
