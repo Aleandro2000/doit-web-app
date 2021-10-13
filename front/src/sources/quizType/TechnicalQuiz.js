49e1af799e9b2cef348729fa0e5c814825465401
Aleandro2000 <carmicialexandru@yahoo.com>
2021-10-13 13:26:56 +0300

    doit

--- front/src/sources/quizType/TechnicalQuiz.js
+++ front/src/sources/quizType/TechnicalQuiz.js
@@ -1,6 +1,5 @@
 import QuizTest from "../../components/QuizTest";
 import Header from "../../components/Header";
-import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 
 export default function TechnicalQuiz()
